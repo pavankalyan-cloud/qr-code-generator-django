71f5dce9dcb067fdb54b190dede906ad23016943
Pavan Kalyan <pavankalyangorrela@gmail.com>
2026-07-20 19:35:00 +0530

    Update login page

--- qrapp/urls.py
+++ qrapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('register/', views.register, name='register'),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', views.register, name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path("api/generate-qr/", views.generate_qr_api, name="generate_qr_api"),
